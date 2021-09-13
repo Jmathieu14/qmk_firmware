0fc58e67aa22b17bdcbfe5f42303164627b50567
Jacques <jmathfb@gmail.com>
2021-09-13 11:01:41 -0400

    start writing tests for keymap.c parser

--- custom/keyboard_switch_and_keymap_layout/parser/keymap_c_parser.py
+++ custom/keyboard_switch_and_keymap_layout/parser/keymap_c_parser.py
@@ -1,10 +1,21 @@
 # Author: Jacques
 # Date: 09/10/2021
 # Time: 4:32 PM
+import os.path as osp
+from custom.keyboard_switch_and_keymap_layout.keyboard_switch_and_keymap_layout import KeyboardSwitchLayout
 
 
-def parse_keymap_c(filepath: str):
-    pass
+def does_file_exist(path):
+    if not path.__str__().find(osp.curdir.__str__()):
+        path = osp.abspath(osp.curdir + "\\" + path)
+    return osp.exists(path)
+
+
+def parse_keymap_c(filepath: str, switch_layout: KeyboardSwitchLayout):
+    if does_file_exist(filepath):
+        pass
+    else:
+        raise FileNotFoundError('Could not find {0}'.format(filepath))
 
 
 def main():
--- custom/keyboard_switch_and_keymap_layout/parser/test_keymap_c_parser.py
+++ custom/keyboard_switch_and_keymap_layout/parser/test_keymap_c_parser.py
@@ -1,1 +1,35 @@
 # TODO: import simple_keymap and make sure you get the expected layers
+
+import unittest
+
+from custom.keyboard_switch_and_keymap_layout.keyboard_switch_and_keymap_layout import KeyboardSwitchRow, KeyboardSwitchLayout, KeyboardLayout, KeyboardKeymap
+from custom.keyboard_switch_and_keymap_layout.parser.keymap_c_parser import parse_keymap_c
+
+switch_row_len_2 = KeyboardSwitchRow(2)
+simple_switch_layout = KeyboardSwitchLayout([switch_row_len_2, switch_row_len_2])
+
+class TestKeymapCParser(unittest.TestCase):
+
+    def test_import_simple_keymap__returns_expected_keyboard_layout_object(self):
+        actual_keyboard_layout = parse_keymap_c('./assets/simple_keymap.c', simple_switch_layout)
+        expected_keyboard_layout = KeyboardLayout(simple_switch_layout, [
+            KeyboardKeymap('0', ['KC_1', 'KC_2', 'KC_3', 'KC_4'])
+        ])
+        self.assertEqual(expected_keyboard_layout, actual_keyboard_layout)
+
+    def test_import_non_existent_file__raises_error(self):
+        with self.assertRaises(FileNotFoundError):
+            parse_keymap_c('./wah/no_file.c', simple_switch_layout)
+
+
+def get_suite():
+    return unittest.TestLoader().loadTestsFromTestCase(TestKeymapCParser)
+
+
+def main():
+    unittest.TextTestRunner().run(get_suite())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
