0c4470edf04afb654be71fb7d3c43e58dd7a70e5
Jacques Mathieu <jmathfb@gmail.com>
2023-02-03 09:45:33 -0500

    Add Colemak DH Mod keymap

--- keyboards/primekb/prime_e/keymaps/jm_game_colemak_modified_dh/keymap.c
+++ keyboards/primekb/prime_e/keymaps/jm_game_colemak_modified_dh/keymap.c
@@ -41,10 +41,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[DEFAULT] = LAYOUT( \
-		LT(NUM,KC_ESC)  , KC_Q          , KC_W          , KC_F          , KC_P          , KC_G          , KC_J                , KC_L          , KC_U          , KC_Y          , KC_SCLN       , KC_DEL         , KC_BSPC, \
-		LT(MEDIA,KC_TAB), KC_A          , KC_S          , KC_R          , KC_T          , KC_D          , KC_H                , KC_N          , KC_E          , KC_I          , KC_O          , TD(NTR_BCKSLSH), \
-		KC_LCTL         , KC_Z          , KC_X          , KC_C          , KC_V          , KC_B          , LT(SYM_FUNC,KC_QUOT), KC_K          , KC_M          , KC_COMM       , KC_DOT        , KC_SLSH        , MO(NUM), \
-		KC_LSFT         , KC_LGUI       , KC_LALT       , LSFT_T(KC_SPC), KC_SPC        , MO(NAV)       , KC_RALT             , KC_RCTL       ),
+		LT(NUM,KC_ESC)  , KC_Q          , KC_W          , KC_F          , KC_P          , KC_B                         , KC_J          , KC_L          , KC_U          , KC_Y          , KC_SCLN       , KC_DEL         , KC_BSPC, \
+		LT(MEDIA,KC_TAB), KC_A          , KC_S          , KC_R          , KC_T          , KC_G                         , KC_M          , KC_N          , KC_E          , KC_I          , KC_O          , TD(NTR_BCKSLSH), \
+		KC_LCTL         , KC_X          , KC_C          , KC_D          , KC_V          , KC_Z   , LT(SYM_FUNC,KC_QUOT), KC_K          , KC_H          , KC_COMM       , KC_DOT        , KC_SLSH        , MO(NUM), \
+		KC_LSFT         , KC_LGUI       , KC_LALT       , LSFT_T(KC_SPC), KC_SPC        , MO(NAV), KC_RALT             , KC_RCTL       ),
 
 	[GAME] = LAYOUT( \
 		KC_TAB , KC_Q   , KC_W   , KC_E   , TD(R_ONE)  , TD(T_FOUR), TD(Y_SEVEN), TD(U_ZERO) , KC_I  , KC_O   , KC_P   , KC_TRNS, KC_TRNS, \
